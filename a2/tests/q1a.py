b2b549d3c07a09647523148d6d8a628a7af06356
Darshan Patil <dapatil211@gmail.com>
2023-10-02 16:32:33 -0400

    Updated test

--- a2/tests/q1a.py
+++ a2/tests/q1a.py
@@ -9,7 +9,7 @@ points = 4
 def test_evaluate_agent_public(gym, np, ExpertAgent, evaluate_agent):
     env = gym.make("Ant-v4")
     env.reset(seed=42)
-    expert_1mil = ExpertAgent("experts/network_1mil.pt")
+    expert_1mil = ExpertAgent("./public/a2/experts/network_1mil.pt")
     mean, std = evaluate_agent(expert_1mil, env, 10)
     assert mean > 4000, "The mean return of the expert should be greater than 4000"
     assert std != 0, "The standard deviation of the expert should not be 0"
--- a2/tests/q3.py
+++ a2/tests/q3.py
@@ -7,7 +7,7 @@ points = 20
 
 @test_case(points=3, hidden=False)
 def test_relabel_trajectories_public(np, ExpertAgent, relabel_with_expert):
-    expert = ExpertAgent("experts/network_1mil.pt")
+    expert = ExpertAgent("./public/a2/experts/network_1mil.pt")
     states = np.random.randn(10, 27)
     actions = relabel_with_expert(states, expert)
     assert actions.shape == (10, 8)
@@ -15,7 +15,7 @@ def test_relabel_trajectories_public(np, ExpertAgent, relabel_with_expert):
 @test_case(points=2, hidden=False)
 def test_collect_rollouts_public(gym, collect_rollouts, ExpertAgent):
     env = gym.make("Ant-v4")
-    expert = ExpertAgent("experts/network_1mil.pt")
+    expert = ExpertAgent("./public/a2/experts/network_1mil.pt")
     states, actions = collect_rollouts(env, expert, 1000)
     assert states.shape == (1000, 27)
     assert actions.shape == (1000, 8)
@@ -25,7 +25,7 @@ def test_seed_data_public(gym, seed_data, ExpertAgent):
     from unittest.mock import Mock
     env = gym.make("Ant-v4")
     buffer = Mock()
-    seed_data(env, ExpertAgent("experts/network_1mil.pt"), buffer, 1000)
+    seed_data(env, ExpertAgent("./public/a2/experts/network_1mil.pt"), buffer, 1000)
     assert buffer.add_rollouts.call_count == 1
     assert buffer.add_rollouts.call_args[0][0]["states"].shape == (1000, 27)
     assert buffer.add_rollouts.call_args[0][0]["actions"].shape == (1000, 8)    
