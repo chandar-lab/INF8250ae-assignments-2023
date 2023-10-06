08ec7fed988c5e80aebc471444e06b9b65902ceb
Darshan Patil <dapatil211@gmail.com>
2023-10-06 13:37:41 -0400

    Updated Q2a test

--- a2/tests/q2.a.py
+++ a2/tests/q2.a.py
@@ -7,8 +7,15 @@ points = 15
 
 @test_case(points=5, hidden=False)
 def test_correct_loss(np, ReplayBuffer, behavior_cloning, torch):
-    from unittest.mock import Mock
-    agent = lambda x: x + torch.nn.parameter.Parameter(torch.Tensor([2., 2.]))
+    from unittest.mock import Mock, MagicMock, patch
+    # agent = lambda x: x + torch.nn.parameter.Parameter(torch.Tensor([2., 2.]))
+    # agent = MagicMock()
+    class MockAgent():
+        def __call__(self, x):
+            return x + torch.nn.parameter.Parameter(torch.Tensor([2., 2.]))
+        def forward(self, x):
+            return x + torch.nn.parameter.Parameter(torch.Tensor([2., 2.]))
+    agent = MockAgent()
     optimizer = Mock()
     buffer = ReplayBuffer()
     buffer.add_rollouts(
